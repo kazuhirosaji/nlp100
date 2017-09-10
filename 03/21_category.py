4659368f83c704b9894bd51b50e3248fee21ac13
kazuhirosaji <kazuhirosaji@gmail.com>
2017-09-10 18:24:08 +0900

    コード整理

--- 03/21_category.py
+++ 03/21_category.py
@@ -4,11 +4,11 @@ import nlp_json
 import re
 
 r = nlp_json.read_wikipedia()
-# for value in r['revisions'][0]['*']:
-    # print(value)
 
 # 文字列から[[Category:~]]を探す
 list = re.findall('Category:.*', r['revisions'][0]['*'])
+
+# 余分な文字を削除
 ans = []
 for matched in list:
     matched = re.sub('Category:', "", matched)
