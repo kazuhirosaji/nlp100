b15629063c2bd2a6fe8ccbfbe3cf99640e646d5d
kazuhirosaji <kazuhirosaji@gmail.com>
2017-11-28 12:51:39 +0900

    課題31. 動詞 の対応

--- 04/31_verb.py
+++ 04/31_verb.py
@@ -1,5 +1,11 @@
 # 31. 動詞
 # 動詞の表層形をすべて抽出せよ．
 import analyze as gal
+list = gal.get_analyzed_list()
 
-print(gal.get_analyzed_list())
+verbs = []
+for res in list:
+    if (res['pos'] == '動詞'):
+        verbs.append(res['surface'])
+
+print(verbs)
